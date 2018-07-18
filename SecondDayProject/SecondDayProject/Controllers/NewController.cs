c87c6215f6f3eff42b318f2a90771b44bd601872
Bryanna Brueggman <briexmac@aol.com>
2018-07-17 19:13:51 -0500

    projects

---
+++ SecondDayProject/SecondDayProject/Controllers/NewController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SecondDayProject.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SecondDayProject.Controllers
+{
+    public class NewController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var model = new Greeting();
+            model.CurrentTime = DateTime.Today.AddHours(5);
+            model.Firstname = "Bryanna";
+            return View(model);
+        }
+    }
+}
---
+++ SecondDayProject/SecondDayProject/Models/EmptyClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace SecondDayProject.Models
+{
+    public class Greeting
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public int Age { get; set; }
+        public string HomeTown { get; set; }
+        public object CurrentTime { get; internal set; }
+    }
+}
