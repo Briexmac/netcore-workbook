1dc3108d79cd45057a1883cdb18f3e0760333cbf
Briexmac <Briexmac@aol.com>
2018-08-18 21:51:32 -0500

    update

--- Lesson04/EntityFrameworkAdded/BaseProject/Controllers/UserController.cs
+++ Lesson04/EntityFrameworkAdded/BaseProject/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using BaseProject.Data;
+using BaseProject.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -20,7 +21,20 @@ namespace BaseProject.Controllers
         // GET: Users
         public async Task<IActionResult> Index()
         {
-            return View(await _context.Users.ToListAsync());
+            return View(await _context.Users);
+        }
+
+
+        [HttpPost]
+        [Route("Create")]
+        // GET: Users
+        public async Task<IActionResult> Create(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return RedirectToAction();
         }
     }
 }
+
+
